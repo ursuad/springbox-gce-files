bee578a19170fab21bab95bc70463076d2cd1f46
Adrian Ursu <Adrian.Ursu@opencredo.com>
2015-11-06 14:29:26 +0000

    Get external IP address and generate file for /etc/hosts

--- scripts/get_hosts_entry.sh
+++ scripts/get_hosts_entry.sh
@@ -8,3 +8,13 @@ header() {
 
 }
 
+AUTH_SERVER_SELECTOR="name=auth-server-frontend"
+header "Getting external addresses for: "
+echo   "      - auth server"
+AUTH_SERVER_HOST=$(kubectl get svc -l ${AUTH_SERVER_SELECTOR} -o json | jq -r  ".items[0].status.loadBalancer.ingress[0].ip")
+AUTH_SERVER_PORT=$( kubectl get svc -l ${AUTH_SERVER_SELECTOR} -o json | jq -r  ".items[0].spec.ports[0].port")
+header "Got following values:"
+echo "- auth server: ${AUTH_SERVER_HOST} at port ${AUTH_SERVER_PORT}"
+
+header "Add the line below at the end of your /etc/hosts file"
+echo "  ${AUTH_SERVER_HOST}  my-auth-server # Added for the Google Container Engine tutorial. Remove after"
