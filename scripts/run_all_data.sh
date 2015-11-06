000f20a88681ac4b35ac436061f60f3c63d6b637
Adrian Ursu <Adrian.Ursu@opencredo.com>
2015-11-06 16:13:16 +0000

    Updated script for populating data.

--- scripts/run_all_data.sh
+++ scripts/run_all_data.sh
@@ -9,37 +9,18 @@ header() {
 }
 
 
-AUTH_SERVER_SELECTOR="name=auth-server-frontend"
-RECOMMENDATIONS_SELECTOR="name=recommendations-frontend"
-REVIEWS_SELECTOR="name=reviews-frontend"
-
-header "Getting external addresses for"
-echo "- auth server"
-echo "- recommendations server"
-echo "- reviews server"
-
-AUTH_SERVER_HOST=$(kubectl get svc -l ${AUTH_SERVER_SELECTOR} -o json | jq -r  ".items[0].status.loadBalancer.ingress[0].ip")
-AUTH_SERVER_PORT=$( kubectl get svc -l ${AUTH_SERVER_SELECTOR} -o json | jq -r  ".items[0].spec.ports[0].port")
-RECOMMENDATIONS_SERVER_HOST=$(kubectl get svc -l ${RECOMMENDATIONS_SELECTOR} -o json | jq -r  ".items[0].status.loadBalancer.ingress[0].ip")
-RECOMMENDATIONS_SERVER_PORT=$( kubectl get svc -l ${RECOMMENDATIONS_SELECTOR} -o json | jq -r  ".items[0].spec.ports[0].port")
-REVIEWS_SERVER_HOST=$(kubectl get svc -l ${REVIEWS_SELECTOR} -o json | jq -r  ".items[0].status.loadBalancer.ingress[0].ip")
-REVIEWS_SERVER_PORT=$(kubectl get svc -l ${REVIEWS_SELECTOR} -o json | jq -r  ".items[0].spec.ports[0].port")
+AUTH_SERVER_HOST="localhost"
+AUTH_SERVER_PORT=$(kubectl get pod -l app=auth-server -o json |  jq -r  ".items[0].spec.containers[0].ports[0].containerPort")
+RECOMMENDATIONS_SERVER_HOST="localhost"
+RECOMMENDATIONS_SERVER_PORT=$(kubectl get pod -l app=springbox-recommendations -o json |  jq -r  ".items[0].spec.containers[0].ports[0].containerPort")
+REVIEWS_SERVER_HOST="localhost"
+REVIEWS_SERVER_PORT=$(kubectl get pod -l app=springbox-reviews -o json |  jq -r  ".items[0].spec.containers[0].ports[0].containerPort")
 
 header "Got following values:"
 echo "- auth server: ${AUTH_SERVER_HOST} at port ${AUTH_SERVER_PORT}"
 echo "- recommendations server: ${RECOMMENDATIONS_SERVER_HOST} at port ${RECOMMENDATIONS_SERVER_PORT}"
 echo "- auth server: ${REVIEWS_SERVER_HOST} at port ${REVIEWS_SERVER_PORT}"
 
-read -r -p "Continue? [y/N] " response
-case $response in
-    [yY][eE][sS]|[yY])
-        echo "Ok, moving on..."
-        ;;
-    *)
-        echo "You're no fun"
-        exit 0;
-        ;;
-esac
 
 header "Getting access token from server..."
 TOKEN=$(curl -X POST -d "grant_type=password&username=mstine&password=secret" http://${AUTH_SERVER_HOST}:${AUTH_SERVER_PORT}/uaa/oauth/token -H "Authorization: Basic YWNtZTphY21lc2VjcmV0" | jq -r '.access_token')
