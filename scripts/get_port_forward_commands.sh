1b7f65abd312deb9d592dc98c5cefb9027d2793b
Adrian Ursu <Adrian.Ursu@opencredo.com>
2015-11-06 15:45:55 +0000

    Script for generating the commands for port forwarding.

---
+++ scripts/get_port_forward_commands.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+header() {
+    echo "================================================================================"
+    echo $@ | sed  -e :a -e 's/^.\{1,77\}$/ & /;ta'
+    echo "================================================================================"
+
+}
+
+header "Getting port-forward commands..."
+AUTH_SERVER_POD_NAME=$(kubectl get pod -l app=auth-server -o json | jq -r  ".items[0].metadata.name")
+AUTH_SERVER_POD_PORT=$(kubectl get pod -l app=auth-server -o json |  jq -r  ".items[0].spec.containers[0].ports[0].containerPort")
+
+REVIEWS_POD_NAME=$(kubectl get pod -l app=springbox-reviews -o json | jq -r  ".items[0].metadata.name")
+REVIEWS_POD_PORT=$(kubectl get pod -l app=springbox-reviews -o json |  jq -r  ".items[0].spec.containers[0].ports[0].containerPort")
+
+RECOMMENDATIONS_POD_NAME=$(kubectl get pod -l app=springbox-recommendations -o json | jq -r  ".items[0].metadata.name")
+RECOMMENDATIONS_POD_PORT=$(kubectl get pod -l app=springbox-recommendations -o json |  jq -r  ".items[0].spec.containers[0].ports[0].containerPort")
+
+header "Run the commands below in separate terminal tabs, because they are blocking"
+echo "kubectl port-forward -p ${AUTH_SERVER_POD_NAME} ${AUTH_SERVER_POD_PORT}"
+echo "kubectl port-forward -p ${REVIEWS_POD_NAME} ${REVIEWS_POD_PORT}"
+echo "kubectl port-forward -p ${RECOMMENDATIONS_POD_NAME} ${RECOMMENDATIONS_POD_PORT}"
