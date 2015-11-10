597a193347d8493745fc64013a36520ece52f4e2
Adrian Ursu <Adrian.Ursu@opencredo.com>
2015-11-10 13:31:45 +0000

    typo

--- scripts/get_port_forward_commands.sh
+++ scripts/get_port_forward_commands.sh
@@ -17,7 +17,7 @@ REVIEWS_POD_PORT=$(kubectl get pod -l app=springbox-reviews -o json |  jq -r  ".
 RECOMMENDATIONS_POD_NAME=$(kubectl get pod -l app=springbox-recommendations -o json | jq -r  ".items[0].metadata.name")
 RECOMMENDATIONS_POD_PORT=$(kubectl get pod -l app=springbox-recommendations -o json |  jq -r  ".items[0].spec.containers[0].ports[0].containerPort")
 
-header "Run the commands below to generate background jobs for port gorwarding"
+header "Run the commands below to generate background jobs for port forwarding"
 echo "nohup kubectl port-forward -p ${AUTH_SERVER_POD_NAME} ${AUTH_SERVER_POD_PORT} &"
 echo "nohup kubectl port-forward -p ${REVIEWS_POD_NAME} ${REVIEWS_POD_PORT} &"
 echo "nohup kubectl port-forward -p ${RECOMMENDATIONS_POD_NAME} ${RECOMMENDATIONS_POD_PORT} &"
